a340352c7135c7fad85885f9060c381cf0e152b1
Truong, Christopher Taylor <christruong@gatech.edu>
2018-10-30 13:05:51 -0400

    Initial Commit

---
+++ Tatl/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tatl
+//
+//  Created by Chris Truong on 10/30/18.
+//  Copyright © 2018 Chris Truong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
