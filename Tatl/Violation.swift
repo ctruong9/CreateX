645efd376ff4b583434a7f36057f408e31f37024
Truong, Christopher Taylor <christruong28@gmail.com>
2018-11-15 17:28:24 -0500

    adding violation object

---
+++ Tatl/Violation.swift
@@ -0,0 +1,13 @@
+//
+//  Violation.swift
+//  Tatl
+//
+//  Created by Chris Truong on 11/15/18.
+//  Copyright © 2018 Chris Truong. All rights reserved.
+//
+
+import UIKit
+
+class Violation: NSObject {
+
+}
